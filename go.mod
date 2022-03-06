9c91703b57236059d0a52f35c525bb040b99644b
umiwatari823 <umiwatari823@gmail.com>
2022-03-06 16:58:20 +0900

    configの設定

---
+++ go.mod
@@ -0,0 +1,7 @@
+module todo_go
+
+go 1.17
+
+require gopkg.in/go-ini/ini.v1 v1.66.4
+
+require github.com/stretchr/testify v1.7.0 // indirect
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"todo_go/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+}
