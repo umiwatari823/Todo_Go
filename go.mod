c11258552322e406daf80ce5515d4324f7956a81
umiwatari823 <umiwatari823@gmail.com>
2022-03-07 21:11:11 +0900

    userテーブルの追加

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.17
 
 require gopkg.in/go-ini/ini.v1 v1.66.4
 
-require github.com/stretchr/testify v1.7.0 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.12 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"todo_go/config"
+	"todo_go/app/models"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
-	log.Println("test")
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
+	// log.Println("test")
+	fmt.Println(models.Db)
 }
